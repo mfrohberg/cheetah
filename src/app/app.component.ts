39bdecf5c864ac4b04d1a6d574aa427865b6c431
Michael Frohberg <mfrohberg@2u.com>
2016-05-14 19:25:42 -0400

    user can see there repos in console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,54 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import '../../public/css/styles.css';
+import {FirebaseAuth, AngularFire} from 'angularfire2';
+import {GithubService} from './services/github.service'
 
 @Component({
   selector: 'app',
   template: require('./app.component.html'),
-  styles: [require('./app.component.css')]
+  styles: [require('./app.component.css')],
+  providers: [GithubService]
 })
 
-export class AppComponent {
-  title: 'The Aristocrats'
+export class AppComponent implements OnInit {
+  title: 'The Aristocrats';
+  username = this.af.auth.getAuth().github.username || '';
+  projects: any = []
+  
+  constructor(
+    public github: GithubService,
+    public auth: FirebaseAuth,
+    public af: AngularFire
+  ) {}
+  
+  login() {
+    this.auth.login()
+      .then(() => {
+        this.username = this.af.auth.getAuth().github.username
+      })  
+  }
+  
+  logout() {
+    this.auth.logout()
+    this.username = '';
+  }
+  
+  findProjects() {
+    return this.github.getUserRepos()
+      .subscribe(
+        repos => {
+          repos.data.map((repo) => {
+            console.log(repo.name)
+          })
+        },
+        err => console.error(err),
+        () => {
+          console.log('done')
+        }
+      )
+  }
+  ngOnInit() {
+    console.log('hey')
+  }
+  
 }
