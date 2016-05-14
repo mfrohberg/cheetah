9efb676580cb8f99c9093b30ada0c2c814e11f67
Michael Frohberg <mfrohberg@2U.com>
2016-05-14 16:31:24 -0400

    setup / bootstraps firebase (#14)

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,22 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent } from './app/app.component';
+import { HTTP_PROVIDERS } from '@angular/http';
+import {FIREBASE_PROVIDERS, 
+  defaultFirebase, 
+  AngularFire, 
+  AuthMethods, 
+  AuthProviders, 
+  firebaseAuthConfig} from 'angularfire2';
 if (process.env.ENV === 'production') {
   enableProdMode();
 }
-bootstrap(AppComponent, []);
+bootstrap(AppComponent, [
+  FIREBASE_PROVIDERS,
+  HTTP_PROVIDERS,
+  defaultFirebase('https://aristocrats.firebaseio.com'),
+  firebaseAuthConfig({
+    provider: AuthProviders.Github,
+    method: AuthMethods.Popup
+  })
+]);
